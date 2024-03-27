991faeffe7753a12f96ea2c4b2615751e66c506c
ScorpionMarcus <marcus.legault@scorpion.co>
2024-03-27 10:01:26 -0700

    Update firebase.js
    createRecord, updateRecord, deleteRecord added

--- api/v1/utils/firebase.js
+++ api/v1/utils/firebase.js
@@ -1,24 +1,52 @@
 const admin = require('firebase-admin');
-// const { collection } = require('firebase/firestore/lite');
 const db = admin.firestore();
 
 const getRecords = async (collection) => {
-    const snapshot = await db.collection(collection).get();
-    return snapshot.docs.map(doc => ({
-        id: doc.id, ...doc.data()
-    }))
+    try {
+        const snapshot = await db.collection(collection).get();
+        return snapshot.docs.map(doc => ({
+            id: doc.id, ...doc.data()
+        }))
+        
+    } catch (error) {
+        console.error("Error getting records: ", error);
+        throw error;
+    }
 }
 
-// const createRecord = async (collection, data) => {
-//     await db.collection(collection).add(data);
-// }
+const createRecord = async (collection, data) => {
+    try {
+        const docRef = await db.collection(collection).add(data);
+        return { id: docRef.id, ...data};
+        
+    } catch (error) {
+        console.error("Error creating record: ", error);
+        throw error;
+    }
+}
 
-// const updateRecord = async (collection, id, data) => {
-//     await db.collection(collection).doc(id).update(data);
-// }
+const updateRecord = async (collection, id, data) => {
+    try {
+        const docRef = db.collection(collection).doc(id);
+        await docRef.update(data);
+        const updatedDoc = await docRef.get();
+        return { id, ...updatedDoc.data };
+        
+    } catch (error) {
+        console.error("Error updating record: ", error);
+        throw error;
+    }
+}
 
-// const deleteRecord = async (collection, id) => {
-//     await db.collection(collection).doc(id).delete();
-// }
+const deleteRecord = async (collection, id) => {
+    try {
+        await db.collection(collection).doc(id).delete();
+        return { id, deleted: true };
+        
+    } catch (error) {
+        console.error("Error deleting record: ", error);
+        throw error;
+    }
+}
 
-module.exports = {getRecords};
+module.exports = {getRecords, createRecord, updateRecord, deleteRecord};
