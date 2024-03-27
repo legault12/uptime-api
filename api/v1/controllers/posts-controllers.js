24723fc82ffa29b2e87ea8893b53acb20935eb1e
ScorpionMarcus <marcus.legault@scorpion.co>
2024-03-27 10:22:05 -0700

    Update posts-controllers.js
    logic for getRecordsController, createRecordsController ,  updateRecordsController, deleteRecordsController

--- api/v1/controllers/posts-controllers.js
+++ api/v1/controllers/posts-controllers.js
@@ -2,15 +2,40 @@ const { createRecord, getRecords, updateRecord, deleteRecord }  = require('../ut
 
 const getRecordsController = async (req, res) => {
     try {
-        const posts = await getRecords('posts')
+        const posts = await getRecords('posts');
         res.status(200).json(posts);
     } catch (error) {
-        
+        res.status(500).json({ error: "Error getting records", details: error.message });
+    }
+};
+
+const createRecordsController = async (req, res) => {
+    try {
+        const newPost = await createRecord('posts', req.body);
+        res.status(201).json(newPost);
+    } catch (error) {
+        res.status(500).json({ error: "Error creating record", details: error.message })
     }
 }
 
-const createRecordsController = (req, res) => {
-    const something = await(createRecord)
+const updateRecordsController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedPost = await updateRecord('posts', id, req.body);
+        res.status(200).json(updatedPost);
+    } catch (error) {
+        res.status(500).json({ error: "Error updating record", details: error.message })
+    }
 }
 
-module.exports = {getRecordsController};
+const deleteRecordsController = async (req, res) => {
+    try {
+        const { id } = req.params;
+        await deleteRecord('posts', id);
+        res.status(200).json({ id, deleted: true });
+    } catch (error) {
+        res.status(500).json({ error: "Error deleting record", details: error.message })
+    }
+}
+
+module.exports = {getRecordsController, createRecordsController, updateRecordsController, deleteRecordsController};
