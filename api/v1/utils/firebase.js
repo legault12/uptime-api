b3c47b5c1ccddba139a846cf9322c4a8c5654558
tdarrow7 <tdarrow7@users.noreply.github.com>
2024-04-15 16:57:57 -0700

    set up .env file and new firebase_config file to pull variables in from

--- api/v1/utils/firebase.js
+++ api/v1/utils/firebase.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,56 +19,114 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.deleteRecord = exports.updateRecord = exports.createRecord = exports.getRecords = void 0;
-const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const db = firebase_admin_1.default.firestore();
-const getRecords = (collection) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const snapshot = yield db.collection(collection).get();
-        return snapshot.docs.map(doc => (Object.assign({ id: doc.id }, doc.data())));
-    }
-    catch (error) {
-        console.error("Error getting records: ", error);
-        throw error;
-    }
-});
+var firebase_admin_1 = require("firebase-admin");
+var db = firebase_admin_1.default.firestore();
+var getRecords = function (collection) { return __awaiter(void 0, void 0, void 0, function () {
+    var snapshot, error_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, db.collection(collection).get()];
+            case 1:
+                snapshot = _a.sent();
+                return [2 /*return*/, snapshot.docs.map(function (doc) { return (__assign({ id: doc.id }, doc.data())); })];
+            case 2:
+                error_1 = _a.sent();
+                console.error("Error getting records: ", error_1);
+                throw error_1;
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
 exports.getRecords = getRecords;
-const createRecord = (collection, data) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const docRef = yield db.collection(collection).add(data);
-        return Object.assign({ id: docRef.id }, data);
-    }
-    catch (error) {
-        console.error("Error creating record: ", error);
-        throw error;
-    }
-});
+var createRecord = function (collection, data) { return __awaiter(void 0, void 0, void 0, function () {
+    var docRef, error_2;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, db.collection(collection).add(data)];
+            case 1:
+                docRef = _a.sent();
+                return [2 /*return*/, __assign({ id: docRef.id }, data)];
+            case 2:
+                error_2 = _a.sent();
+                console.error("Error creating record: ", error_2);
+                throw error_2;
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
 exports.createRecord = createRecord;
-const updateRecord = (collection, id, data) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const docRef = db.collection(collection).doc(id);
-        yield docRef.update(data);
-        const updatedDoc = yield docRef.get();
-        return Object.assign({ id }, updatedDoc.data());
-    }
-    catch (error) {
-        console.error("Error updating record: ", error);
-        throw error;
-    }
-});
+var updateRecord = function (collection, id, data) { return __awaiter(void 0, void 0, void 0, function () {
+    var docRef, updatedDoc, error_3;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 3, , 4]);
+                docRef = db.collection(collection).doc(id);
+                return [4 /*yield*/, docRef.update(data)];
+            case 1:
+                _a.sent();
+                return [4 /*yield*/, docRef.get()];
+            case 2:
+                updatedDoc = _a.sent();
+                return [2 /*return*/, __assign({ id: id }, updatedDoc.data())];
+            case 3:
+                error_3 = _a.sent();
+                console.error("Error updating record: ", error_3);
+                throw error_3;
+            case 4: return [2 /*return*/];
+        }
+    });
+}); };
 exports.updateRecord = updateRecord;
-const deleteRecord = (collection, id) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        yield db.collection(collection).doc(id).delete();
-        return { id, deleted: true };
-    }
-    catch (error) {
-        console.error("Error deleting record: ", error);
-        throw error;
-    }
-});
+var deleteRecord = function (collection, id) { return __awaiter(void 0, void 0, void 0, function () {
+    var error_4;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                _a.trys.push([0, 2, , 3]);
+                return [4 /*yield*/, db.collection(collection).doc(id).delete()];
+            case 1:
+                _a.sent();
+                return [2 /*return*/, { id: id, deleted: true }];
+            case 2:
+                error_4 = _a.sent();
+                console.error("Error deleting record: ", error_4);
+                throw error_4;
+            case 3: return [2 /*return*/];
+        }
+    });
+}); };
 exports.deleteRecord = deleteRecord;
