7b551ee140c80aafd5374a5ae7e054a00ed65a89
tdarrow7 <tdarrow7@users.noreply.github.com>
2024-04-15 17:31:25 -0700

    converted monitor dto

--- api/v1/monitors/dtos/monitors.ts
+++ api/v1/monitors/dtos/monitors.ts
@@ -1,41 +1,57 @@
-// TODO: Enhance WebsiteStatus Model
-// 1. Further define the structure and properties as project requirements become clearer. Consider potential additional fields related to website monitoring and analysis.
-// 2. Implement data validation within the model to ensure the integrity of the data being processed and stored.
-// 3. Integrate this model into the controllers. Update controller logic to utilize this model for creating, updating, and processing website status data.
-// 4. Review and potentially add methods that might be necessary for handling more complex operations or calculations related to website status.
-// Note: This is an initial model structure and is subject to change as the application requirements evolve.
 
-class WebsiteStatus {
-    url: string;
-    isUp: boolean;
-    outages: number;
-    uptime: number;
-    downtime: number;
 
-    constructor(url: string, isUp: boolean, outages: number, uptime: number, downtime: number) {
-        this.url = url;
-        this.isUp = isUp;
-        this.outages = outages;
-        this.uptime = uptime;
-        this.downtime = downtime;
-    }
-
-    updateStatus({ isUp, outages, uptime, downtime }: { isUp?: boolean, outages?: number, uptime?: number, downtime?: number }): void {
-        this.isUp = isUp !== undefined ? isUp : this.isUp;
-        this.outages = outages !== undefined ? outages : this.outages;
-        this.uptime = uptime !== undefined ? uptime : this.uptime;
-        this.downtime = downtime !== undefined ? downtime : this.downtime;
-    }
-
-    toFirestore(): { url: string, isUp: boolean, outages: number, uptime: number, downtime: number } {
-        return {
-            url: this.url,
-            isUp: this.isUp,
-            outages: this.outages,
-            uptime: this.uptime,
-            downtime: this.downtime,
+interface Monitor {
+    id: string;
+    relationships?: {
+      policy: {
+        data: {
+          id: string;
+          type: string;
         };
-    }
+      };
+    };
+    attributes: MonitorAttributes;
+    type: string;
+  }
+  
+interface MonitorAttributes {
+    paused: boolean;
+    regions: string[];
+    policy_id: string;
+    request_timeout: number;
+    remember_cookies: boolean;
+    maintenance_days: string[];
+    expected_status_codes: number[];
+    created_at: string;
+    playwright_script?: string;
+    team_wait: number;
+    maintenance_from?: string;
+    pronounceable_name: string;
+    http_method: string;
+    paused_at?: string;
+    updated_at: string;
+    auth_username: string;
+    confirmation_period: number;
+    sms: boolean;
+    monitor_group_id: number;
+    follow_redirects: boolean;
+    email: boolean;
+    domain_expiration: number;
+    ssl_expiration: number;
+    check_frequency: number;
+    auth_password: string;
+    recovery_period: number;
+    url: string;
+    push: boolean;
+    last_checked_at: string;
+    maintenance_timezone: string;
+    call: boolean;
+    required_keyword?: string;
+    verify_ssl: boolean;
+    request_body: string;
+    port?: number;
+    maintenance_to?: string;
+    request_headers: string[];
+    monitor_type: string;
+    status: string;
 }
-
-export default WebsiteStatus;
